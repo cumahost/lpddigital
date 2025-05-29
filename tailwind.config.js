c0d7d9cede7f8a25cca47b02257ce769e61f6107
cumahost <cumahost@gmail.com>
2025-05-29 23:11:11 +0800

    tailwindcss kedua

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
- /** @type {import('tailwindcss').Config} */
-export default {
-   content: ["./src/**/*.{html,js}"],
-   theme: {
-     extend: {},
-   },
-   plugins: [],
- }
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0a3d62',     // biru tua
+        success: '#2ecc71',     // hijau desa
+        gold: '#f1c40f'         // emas
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif']
+      }
+    }
+  },
+  plugins: [],
+}
