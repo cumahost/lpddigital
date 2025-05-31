07ffb5f1225e3e4d855bd9ae649f6c1e2f048f44
cumahost <cumahost@gmail.com>
2025-05-31 18:27:25 +0800

    edit beranda dengan hero baru

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./*.html"
+    "./*.html",
+    "./**/*.html",
+    "./js/**/*.js",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: '#0a3d62',     // biru tua
-        success: '#2ecc71',     // hijau desa
-        gold: '#f1c40f'         // emas
-      },
-      fontFamily: {
-        inter: ['Inter', 'sans-serif']
-      }
-    }
+    extend: {},
   },
   plugins: [],
-}
+};
